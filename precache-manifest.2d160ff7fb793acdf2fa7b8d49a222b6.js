a14aba620244e7e4a5bcc01104454a60af47f38d
CWood <caitlinjoannawood@gmail.com>
2018-12-06 21:09:46 -0500

    Updates

---
+++ precache-manifest.2d160ff7fb793acdf2fa7b8d49a222b6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "177e1b728fb0d4ffdfea",
+    "url": "/weather/static/js/runtime~main.177e1b72.js"
+  },
+  {
+    "revision": "85f1f5b4212ded2fdeaa",
+    "url": "/weather/static/js/main.85f1f5b4.chunk.js"
+  },
+  {
+    "revision": "725dbc44b25238e5e16d",
+    "url": "/weather/static/js/1.725dbc44.chunk.js"
+  },
+  {
+    "revision": "85f1f5b4212ded2fdeaa",
+    "url": "/weather/static/css/main.f1e2a400.chunk.css"
+  },
+  {
+    "revision": "959a9c5db6c1628231e4c791cedb3d9a",
+    "url": "/weather/index.html"
+  }
+];
